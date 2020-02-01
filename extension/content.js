6f9767f07a9edeb8df5cdfc3ab5206322d6e2c9c
Andrei Cristian Petcu <andrei@ceata.org>
2020-02-01 04:01:55 +0200

    port to Firefox

--- extension/content.js
+++ extension/content.js
@@ -5,6 +5,6 @@ console.log("Stop Slide Scrolling in Google Presentation loaded");
 /**
  * 	Listen for scroll, prevent it
  */
-window.addEventListener("mousewheel", function(e) {
+window.addEventListener("DOMMouseScroll", function(e) {
     e.stopPropagation();
 }, true);
